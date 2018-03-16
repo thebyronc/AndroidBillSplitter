f1c71345d1d2bd90dbf8639ba5f7a3f76d448f84
Byron Chang <thebyronc@gmail.com>
2018-03-16 16:24:41 -0700

    add PeopleArrayAdapter

--- app/src/main/java/foiled/androidbillsplitter/PeopleActivity.java
+++ app/src/main/java/foiled/androidbillsplitter/PeopleActivity.java
@@ -1,13 +1,60 @@
 package foiled.androidbillsplitter;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PeopleActivity extends AppCompatActivity {
+    @BindView(R.id.peopleTitleTextView) TextView peopleTitleTextView;
+    @BindView(R.id.addPeopleTextView) TextView mAddPeopleTextView;
+    @BindView(R.id.peopleEditText) TextView mPeopleEditText;
+    @BindView(R.id.addPeopleButton) Button mAddPeopleButton;
+    @BindView(R.id.peopleListView) ListView mPeopleListView;
+
+    public ArrayList<String> peoples = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people);
+        ButterKnife.bind(this);
+
+        peoples.add("Byron Chang");
+        peoples.add("Christine Yin");
+        peoples.add("Tasha Chang");
+        peoples.add("Ken Han-Liang");
+
+        Typeface bebasNeueBold = Typeface.createFromAsset(getAssets(), "fonts/BebasNeue Bold.ttf");
+        peopleTitleTextView.setTypeface(bebasNeueBold);
+
+        mPeopleEditText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mPeopleEditText.setText("");
+            }
+        });
+
+        mAddPeopleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newPerson = mPeopleEditText.getText().toString();
+                peoples.add(newPerson);
+                Toast.makeText(PeopleActivity.this, newPerson + " added.", Toast.LENGTH_LONG).show();
+                mPeopleEditText.setText("");
+            }
+        });
+
+        PeopleArrayAdapter adapter = new PeopleArrayAdapter(this, android.R.layout.simple_list_item_1, peoples);
+        mPeopleListView.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/foiled/androidbillsplitter/PeopleArrayAdapter.java
@@ -0,0 +1,30 @@
+package foiled.androidbillsplitter;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sheep on 3/16/2018.
+ */
+
+public class PeopleArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private ArrayList<String> mPeoples;
+
+    public PeopleArrayAdapter(Context mContext, int resource, ArrayList<String> mPeoples) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mPeoples = mPeoples;
+    }
+    @Override
+    public Object getItem(int position) {
+        String bill = mPeoples.get(position);
+        return String.format(bill);
+    }
+    @Override
+    public int getCount() {
+        return mPeoples.size();
+    }
+}
