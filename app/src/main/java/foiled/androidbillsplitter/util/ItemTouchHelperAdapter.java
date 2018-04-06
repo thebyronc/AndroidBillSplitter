ce5e6543a68131f4dbed186dfd1076f2f5f519f4
Byron Chang <thebyronc@gmail.com>
2018-04-06 10:23:07 -0700

    add util package

---
+++ app/src/main/java/foiled/androidbillsplitter/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,6 @@
+package foiled.androidbillsplitter.util;
+
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
